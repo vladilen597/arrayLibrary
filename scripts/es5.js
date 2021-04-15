03262b5d8859fbc3675a7986fb6bba7b57126487
Uladzilen Isayenia <Uladzilen.Isayenia@itechart-group.com>
2021-04-15 14:34:37 +0300

    Module function update

--- scripts/es5.js
+++ scripts/es5.js
@@ -1,78 +1,34 @@
-let arrayLibrary = {
-    error: "Handling error. Check the inputs.",
-    arrayToReturn: [],
+function arrayLibrary() {
+    let error = "Handling error. Check the inputs.";
+    let arrayToReturn = [];
 
 
-    chain(array) {
-        return {
-            error: "Handling error. Check the inputs.",
-            arrayToReturn: [],
-
-            take(num) {
-                let innerArray = [];
-                if (this.arrayToReturn.length < num) {
-                    return this.error;
-                }
-
-                for (let i = 0; i < num; i++) {
-                    innerArray[i] = this.arrayToReturn[i];
-                }
-
-                this.arrayToReturn = [];
-                for (let i = 0; i < num; i++) {
-                    this.arrayToReturn[i] = innerArray[i];
-                }
-
-                return this;
-            },
-
-            skip(num) {
-                this.arrayToReturn = array;
-                if (array.length < num) {
-                    return this.error;
-                }
-
-                this.arrayToReturn = [];
-                let j = 0;
-                for (let i = num; i < array.length; i++) {
-                    this.arrayToReturn[j] = array[i];
-                    j++;
-                }
-                return this;
-            },
-
-            getValue() {
-                return this.arrayToReturn;
-            }
-        }
-    },
-
-    take(array, n) { // Берет из входного массива элементы до n 
+    function take(array, n) { // Берет из входного массива элементы до n 
         if (array.length < n) {
-            return this.error;
+            return error;
         }
 
         for (let i = 0; i < n; i++) {
-            this.arrayToReturn[i] = array[i];
+            arrayToReturn[i] = array[i];
         }
-        return this.arrayToReturn;
-    },
+        return arrayToReturn;
+    };
 
-    skip: function(array, n) { // Пропускает элементы до n
-        this.arrayToReturn = [];
+    function skip(array, n) { // Пропускает элементы до n
+        arrayToReturn = [];
         if (array.length < n) {
-            return this.error;
+            return error;
         }
 
         let j = 0;
         for (let i = n; i < array.length; i++) {
-            this.arrayToReturn[j] = array[i]
+            arrayToReturn[j] = array[i]
             j++;
         }
-        return this.arrayToReturn;
-    },
+        return arrayToReturn;
+    };
 
-    map: function(array, callback) { // Обработка массива введенной функцией
+    function map(array, callback) { // Обработка массива введенной функцией
         let arrayToReturn = [];
 
         for (let i = 0; i < array.length; i++) {
@@ -80,23 +36,22 @@ let arrayLibrary = {
             arrayToReturn.push(response);
         }
         return arrayToReturn;
-    },
+    };
 
-    reduce: function(array, callback, initialValue = 0) {
+    function reduce(array, callback, initialValue = 0) {
         let total = initialValue;
 
         if (initialValue == 0) {
             total = array.shift();
         }
 
-
         for (let i = 0; i < array.length; i++) {
             total = callback(total, array[i]);
         }
         return total;
-    },
+    };
 
-    filter: function(array, callback) { // Возвращает новый массив из указанного условия
+    function filter(array, callback) { // Возвращает новый массив из указанного условия
         let newArray = [];
 
         for (let i = 0; i < array.length; i++) {
@@ -106,33 +61,39 @@ let arrayLibrary = {
             }
         }
         return newArray;
-    },
+    };
 
-    foreach: function(array, callback) {
+    function foreach(array, callback) {
         let response;
+
         for (let i = 0; i < array.length; i++) {
             response = callback(array[i], i, array);
         }
         return response;
-    },
+    };
+
+    return {
+        take: take,
+        skip: skip,
+        map: map,
+        reduce: reduce,
+        filter: filter,
+        foreach: foreach
+    }
 }
+
+// console.log("Take: " + arrayLibrary.take(array, n));
+// console.log("Skip: " + arrayLibrary.skip(array, n));
+// console.log("Map: " + arrayLibrary.map(array, (a) => { return a * 2 }));
+// console.log("Reduce: " + arrayLibrary.reduce(array, (a, b) => { return (a += b) }, 5));
+// console.log("Filter: " + arrayLibrary.filter(array, (a) => { return a > 3 }));
+// arrayLibrary.foreach(array, (a) => { console.log(`Я - число ${a} из foreach плюс 10 = ` + (a + 10)); });
 
 
 
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let n = 3;
 
-console.log("Take: " + arrayLibrary.take(array, n));
-console.log("Skip: " + arrayLibrary.skip(array, n));
+let arrayLib = new arrayLibrary();
 
-
-console.log("Map: " + arrayLibrary.map(array, (a) => { return a * 2 }));
-
-console.log("Reduce: " + arrayLibrary.reduce(array, (a, b) => { return (a += b) }, 5));
-
-console.log("Filter: " + arrayLibrary.filter(array, (a) => { return a > 3 }));
-
-console.log("");
-
-arrayLibrary.foreach(array, (a) => { console.log(`Я - число ${a} из foreach плюс 10 = ` + (a + 10)); });
-console.log(arrayLibrary.chain(array).skip(4).take(3).getValue());
+arrayLib.reduce(array, (a, b) => { return console.log(a += b) });
