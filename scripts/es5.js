33b60ae28997e6a31711012f0392a7b6526ad40c
Uladzilen Isayenia <Uladzilen.Isayenia@itechart-group.com>
2021-04-19 15:32:29 +0300

    attempt to make the chain system

--- scripts/es5.js
+++ scripts/es5.js
@@ -2,114 +2,28 @@ let arrayLibrary = function() {
     let error = "Handling error. Check the inputs. Maybe you're out of array";
     let arrayToReturn = [];
     let arrayFunction = [];
+    let chainArray = [];
     let num;
 
     function chain(array) {
-        arrayFunction = array;
-
-        function take(n) { // Берет из входного массива элементы до n 
-            if (arrayFunction.length < n) {
-                throw error;
-            }
-            for (let i = 0; i < n; i++) {
-                arrayToReturn[i] = arrayFunction[i];
-            }
-            arrayFunction = arrayToReturn;
-            return this;
-        };
-
-        function skip(n) { // Пропускает элементы до n
-            if (arrayFunction.length < n) {
-                throw error;
-            }
-            let arrayToReturn = [];
-
-            let j = 0;
-            for (let i = n; i < arrayFunction.length; i++) {
-                arrayToReturn[j] = arrayFunction[i];
-                j++;
-            }
-            arrayFunction = arrayToReturn;
-            return this;
-        };
-
-        function map(callback) { // Обработка массива введенной функцией
-            let arrayToReturn = [];
-
-            for (let i = 0; i < arrayFunction.length; i++) {
-                let response = callback(arrayFunction[i], i, arrayFunction);
-                arrayToReturn.push(response);
-            }
-            arrayFunction = arrayToReturn;
-            return this;
-        };
-
-        function reduce(callback, initialValue = 0) {
-            let total = initialValue;
-
-            if (initialValue == 0) {
-                total = arrayFunction.shift();
-            }
-
-            for (let i = 0; i < arrayFunction.length; i++) {
-                total = callback(total, arrayFunction[i]);
-            }
-            arrayFunction = total;
-            return this;
-        };
-
-        function filter(callback) { // Возвращает новый массив из указанного условия
-            let newArray = [];
-
-            for (let i = 0; i < arrayFunction.length; i++) {
-                let response = callback(arrayFunction[i], i, arrayFunction);
-                if (response == true) {
-                    newArray.push(arrayFunction[i]);
-                }
-            }
-            arrayFunction = newArray;
-            return this;
-        };
-
-        function foreach(callback) {
-            let response = [];
-            for (let i = 0; i < arrayFunction.length; i++) {
-                response[i] = callback(arrayFunction[i], i, arrayFunction);
-            }
-            arrayFunction = response;
-            return this;
-        };
-
-        function getValue() {
-            console.log(arrayFunction);
-            return arrayFunction;
-        }
-
+        let arrResult = array;
         return {
-            take: take,
-            skip: skip,
-            reduce: reduce,
-            filter: filter,
-            map: map,
-            foreach: foreach,
-            getValue: getValue
-        }
+            take: function(n) {
+                chainArray.push(take(arrResult, n));
+                return this;
+            },
+            skip: function(n) {
+                chainArray.push(skip(arrResult, n));
+                return this;
+            },
+            getValue: function() {
+                console.log(chainArray);
+            }
+        };
     };
 
-    // function take(n) { // Берет из входного массива элементы до n 
-    //     if (arrayFunction.length < n) {
-    //         throw error;
-    //     }
-
-    //     for (let i = 0; i < n; i++) {
-    //         arrayToReturn[i] = arrayFunction[i];
-    //     }
-    //     arrayFunction = arrayToReturn;
-    //     console.log(arrayFunction);
-    //     return arrayFunction;
-    // };
-
-    function take(array, n) { // Берет из входного массива элементы до n 
+    function take(array, n) { // Берет из входного массива элементы до n
+        arrayFunction = array;
         if (array.length < n) {
             throw error;
         }
@@ -192,7 +106,6 @@ let arrayLibrary = function() {
     }
 }
 
-// const lib = arrayLibrary();
-// let arr = [1, 2, 3, 4, 5, 6, 7];
-// console.log(lib.foreach(arr, (a) => { return a += 10 }));
-module.exports = arrayLibrary;
+const lib = arrayLibrary();
+let arr = [1, 2, 3, 4, 5, 6, 7];
+lib.chain(arr).take(3).skip(1).getValue();
